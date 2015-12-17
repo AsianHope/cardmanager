02f482e09761b29c36a14d8dbe66581db44011fc
Solida Sang <ssang@asianhope.org>
2015-12-17 16:39:50 +0700

    LCR-42 #comment create test data

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 Meteor.startup(function(){
   var existing_cards = Cards.findOne();
+  var existing_scans = Scans.findOne();
+  var existing_actions = Actions.findOne();
   //if there's no data, please load some test data
   if(!existing_cards){
     var cards = JSON.parse(Assets.getText('fixtures/cards.json'));
@@ -7,8 +9,19 @@ Meteor.startup(function(){
       Cards.insert(card);
     });
   }
+  if(!existing_scans){
+    var scans = JSON.parse(Assets.getText('fixtures/scans.json'));
+    _.each(scans, function(scan){
+      Scans.insert(scan);
+    });
+  }
+  if(!existing_actions){
+    var actions = JSON.parse(Assets.getText('fixtures/actions.json'));
+    _.each(actions, function(action){
+      Actions.insert(action);
+    });
+  }
 });
-
 
 Meteor.methods({
   getCard: function(text){
