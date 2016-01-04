41747c4349b891df88ceec83d95e924dcb606e53
Solida Sang <ssang@asianhope.org>
2016-01-04 08:08:12 +0700

    LCR-15 #comment filter card by type on  card currently on campus

--- client/admin/controller/card-currently-on-campus.js
+++ client/admin/controller/card-currently-on-campus.js
@@ -1,7 +1,16 @@
 Template.cardCurrentlyOnCampus.helpers({
   cards_currently_on_campus: function () {
+    var badge_type = Session.get("badge_type_on_campus_now");
+    if(badge_type){
+      var badges = Cards.find({'type':badge_type}).fetch();
+    }
+    else {
+      var badges = Cards.find({}).fetch();
+    }
+    var barcodes = badges.map(function(card) { return card.barcode });
     cards =  Scans.find({
         $and :[
+          {'cardnumber': {$in: barcodes}},
           {'action':'Security Scan'},
           {'scantimes.1':null}
         ]
@@ -9,22 +18,19 @@ Template.cardCurrentlyOnCampus.helpers({
       {sort: {scantimes : -1}}).fetch()
     return cards
   },
-  cards_currently_on_campus_total: function () {
-    cards_total =  Scans.find({
-        $and :[
-          {'action':'Security Scan'},
-          {'scantimes.1':null}
-        ]
-      },
-      {sort: {scantimes : -1}}).count()
-    return cards_total
-  },
   getCardByBarcode:function(cardnumber){
     var result = Cards.findOne({'barcode':cardnumber});
     return result;
   },
+  badge_type : function(){
+    return Session.get('badge_type_on_campus_now');
+  }
 });
 
 Template.cardCurrentlyOnCampus.events({
-
+  "change #filter_by_badge_type": function (e) {
+    e.preventDefault();
+    var badge_type = $(e.target).val();
+    Session.set("badge_type_on_campus_now",badge_type);
+  }
 });
