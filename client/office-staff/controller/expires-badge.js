de82f554b1bdafde4905d808f13db8903edc8b25
zalen <zalen@trellon.com>
2016-01-27 00:05:20 -0500

    LCR-29 #comment add renew button to expire/renew badge list

--- client/office-staff/controller/expires-badge.js
+++ client/office-staff/controller/expires-badge.js
@@ -18,5 +18,15 @@ Template.expiresBadge.events({
     var id = $(e.currentTarget).data('id');
     var today =  moment(new Date()).format("YYYY-MM-D");
     Meteor.call('expire_card',id,today);
+  },
+  'click .renew': function(e, t) {
+    var id = $(e.currentTarget).data('id');
+
+    var expire_period = $("#renew_period").val() ? $("#renew_period").val() : 1;
+    var now = new Date();
+    var date_after = new Date();
+    date_after.setMonth(now.getMonth() + expire_period);
+    var expire_date = moment(date_after).format("YYYY-MM-DD");
+    Meteor.call('expire_card',id,expire_date);
   }
 });
