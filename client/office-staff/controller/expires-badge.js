e77ccc5ea9df6eebb8c6131a36faf84faafadc7e
zalen <zalen@trellon.com>
2016-02-11 23:29:55 -0500

    LCR-29: fix bug in expire date

--- client/office-staff/controller/expires-badge.js
+++ client/office-staff/controller/expires-badge.js
@@ -23,9 +23,9 @@ Template.expiresBadge.events({
     var id = $(e.currentTarget).data('id');
 
     var expire_period = $("#renew_period").val() ? $("#renew_period").val() : 1;
-    var now = new Date();
-    var date_after = new Date();
-    date_after.setMonth(now.getMonth() + expire_period);
+    var now = moment(new Date());//new Date();
+    var date_after = now.add(expire_period, 'months');//new Date();
+    //date_after.setMonth(now.getMonth() + expire_period);
     var expire_date = moment(date_after).format("YYYY-MM-DD");
     Meteor.call('expire_card',id,expire_date);
   }
